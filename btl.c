4320b6697121da3bb47eee83e0860ce03f73b662
hvqDevUnity <hoangvanquan2002.untydevelop@gmail.com>
2022-10-28 20:02:42 +0700

    Fix bug NULL

--- btl.c
+++ btl.c
@@ -70,13 +70,30 @@ void PrintCmt(){
     printf("\n=--------------------------------------------------------=");
 }
 
-Data *NhapData(Data *data){
+//New
+int IsAvailable(List *list, int maCB){
+    Node *p = list->pHead;
+    while(p != NULL){
+        if( p->data.maCB == maCB)
+            return 0;
+        p = p->nNext;
+    }
+    return 1;
+}
+
+Data *NhapData(List *list, Data *data){
     
-    printf("\nNhap maCB: ");
-    scanf("%d", &data->maCB);
+    //New
+    do{
+        printf("\nNhap maCB: ");
+        scanf("%d", &data->maCB);
+        
+        if(data->maCB < 1) return NULL;
+        if(IsAvailable(list, data->maCB) == 1) break;
+        else printf("MaCB is't avaiable!\n");
+    }while(1);
+
     fflush(stdout);
-    if(data->maCB < 1) return NULL;
-    
     printf("Nhap ho ten: ");
     getchar();
     gets(data->hoTen);
@@ -259,14 +276,22 @@ void InsertByPos(List *list){
 
     Data *data;
     data = (Data*)malloc(sizeof(Data));
-    data = NhapData(data);
 
+    //New
+    data = NhapData(list, data);
+    
+    //New
+    if(data == NULL) {
+        printf("\nBan da huy");
+        return;
+    }
     InsertPos(list, data, pos);
 }
 
 void ScanInput(List *list, Data *data){ 
     
-    data = NhapData(data);
+    //New
+    data = NhapData(list, data);
     if(data == NULL) 
         return;   
     else    
@@ -291,6 +316,9 @@ void PrintMenu(){
 int main(){   
     int choose = -1;
     int n = 0;
+
+    //New
+    int isScan = 0;
     Node *p;
     List *list = (List*)malloc(sizeof(List));
     CreateList(list);   
@@ -299,8 +327,18 @@ int main(){
         PrintMenu();
         scanf("%d", &choose);
 
+        //New
+        if(choose != 1 && isScan == 0){
+            printf("\nMoi ban nhap danh sach.");
+            continue;
+        }else if(choose == 1 && isScan == 1){
+            printf("\nBan da nhap danh sach.");
+            continue;
+        }
+
         if(choose == 1){  		
     		ScanInput(list, data);   
+            isScan = 1;
         }
         else if(choose == 2){
             InsertByPos(list);
